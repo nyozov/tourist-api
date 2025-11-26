43f56139115c461a097c90fec235ea866674e7e0
nikolay <n_yozov@hotmail.com>
2025-11-25 21:55:59 -0500

    Initial commit - cleaned history

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      token = encode_token({ user_id: user.id })
+      render json: { user: user, token: token }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+end
